0402def2152eeb549026becc233ebe3e5a238878
Zordon <alexbajger999@gmail.com>
2022-04-23 19:40:17 +0200

    BIG UPDATE 1.2
    Changelog
    - Added 14 new songs
    - fixes
    - changed TODO

--- README.md
+++ README.md
@@ -17,18 +17,29 @@ you don't must do other steps if you too lazy just use start.cmd
 # TODO
 
 - [x] Make basic music player
-- [ ] Add NCS music
+- [ ] Add most popular NCS music
     - [x] Lost Sky - Fearless [NCS Release]
     - [x] Cartoon - C U Again feat. Mikk Mäe (Cartoon vs Futuristik VIP) [NCS Release]
     - [x] Cartoon - On & On (feat. Daniel Levi) [NCS Release]
     - [x] Lost Sky - Dreams pt. II (feat. Sara Skinner) [NCS Release]
     - [x] Tobu - Infectious [NCS Release]
     - [x] Cartoon - Why We Lose (feat. Coleman Trapp) [NCS Release]
-    - [ ] Cartoon - Howling (Ft. Asena) 
-    - [ ] JPB - High (feat. Aleesia)
-    - [ ] Tobu - Hope [NCS Release]
-    - [ ] Janji feat. TR - Milky Way Stars [NCS Release]
-    - [ ] Tobu - Candyland [NCS Release]
-    - [ ] Different Heaven & EH!DE - My Heart [NCS Release]
+    - [x] Cartoon - Howling (Ft. Asena) 
+    - [x] JPB - High 
+    - [x] Tobu - Hope [NCS Release]
+    - [+] Tobu - Candyland [NCS Release]
+    - [x] Different Heaven & EH!DE - My Heart [NCS Release]
+    - [x] Elektronomia - Sky High [NCS Release]
+    - [x] Lost Sky - Dreams
+    - [x] Egzod & Maestro Chives - Royalty (ft. Neoni) [NCS Release]
+    - [x] Unknown Brain - Perfect 10 (feat. Heather Sommer)
+    - [x] NIVIRO - Flashes [NCS Release]
+    - [x] Jim Yosef - Eclipse [NCS Release]
+    - [x] Desmeon - Undone (feat. Steklo) [NCS Release]
+    - [x] Ahrix - Nova
+    - [x] Earth - K-391
+    - [x] DESMEON - HELLCAT [NCS RELEASE]
+    - [x] DESMEON - UNDONE (FEAT. STEKLO) [NCS RELEASE]
+    - [ ] Nekozilla - Different Heaven
     
 
--- main.py
+++ main.py
@@ -32,6 +32,68 @@ def whywelose():
     pygame.mixer.music.load("./music/whyweloss.wav")
     pygame.mixer.music.play() 
     
+def candyland():
+    pygame.mixer.music.load("./music/candyland.wav")
+    pygame.mixer.music.play() 
+    
+def Flashes():
+    pygame.mixer.music.load("./music/Flashes.wav")
+    pygame.mixer.music.play() 
+
+def Royality():
+    pygame.mixer.music.load("./music/Royalty.wav")
+    pygame.mixer.music.play() 
+
+def myheart():
+    pygame.mixer.music.load("./music/myheart.wav")
+    pygame.mixer.music.play() 
+    
+def Dreams():
+    pygame.mixer.music.load("./music/Dreams.wav")
+    pygame.mixer.music.play() 
+    
+def perfect10():
+    pygame.mixer.music.load("./music/perfect10.wav")
+    pygame.mixer.music.play() 
+
+def howling():
+    pygame.mixer.music.load("./music/howling.wav")
+    pygame.mixer.music.play() 
+    
+def skyhigh():
+    pygame.mixer.music.load("./music/skyhigh.wav")
+    pygame.mixer.music.play() 
+    
+def hope():
+    pygame.mixer.music.load("./music/hope.wav")
+    pygame.mixer.music.play() 
+    
+def high():
+    pygame.mixer.music.load("./music/high.wav")
+    pygame.mixer.music.play() 
+    
+def Eclipse():
+    pygame.mixer.music.load("./music/Eclipse.wav")
+    pygame.mixer.music.play() 
+def nova():
+    pygame.mixer.music.load("./music/nova.wav")
+    pygame.mixer.music.play() 
+    
+def Hellcat():
+    pygame.mixer.music.load("./music/hellcat.wav")
+    pygame.mixer.music.play()
+    
+def Earth():
+    pygame.mixer.music.load("./music/earth.wav")
+    pygame.mixer.music.play()
+    
+def undone():
+    pygame.mixer.music.load("./music/undone.wav")
+    pygame.mixer.music.play()
+  
+def stop():
+    pygame.mixer.music.stop() 
+    
 
 dpg.create_context()
 
@@ -39,14 +101,31 @@ dpg.create_viewport()
 
 dpg.setup_dearpygui()
 
-with dpg.window(label="NCS MUSIC PLAYER - Beta v0.2", pos=(425, 150), width=480, height=480) as main_window:
+with dpg.window(label="NCS MUSIC PLAYER - 1.2", pos=(425, 150), width=480, height=480) as main_window:
     
     dpg.add_text("Select music that you want play")
     dpg.add_button(label="Lost Sky - Fearless pt.II", callback=Fearless)
     dpg.add_button(label="Cartoon - C U Again feat. Mikk Mäe", callback=cuagain)
     dpg.add_button(label="Tobu - Infectious", callback=infectious)
-    dpg.add_button(label="Cartoon - On & On (feat. Daniel Levi) [", callback=ONON)
+    dpg.add_button(label="Cartoon - On & On (feat. Daniel Levi)", callback=ONON)
     dpg.add_button(label="Cartoon - Why We Lose (feat. Coleman Trapp)", callback=whywelose)
+    dpg.add_button(label="Tobu - Candyland [NCS Release]", callback=candyland)
+    dpg.add_button(label="NIVIRO - Flashes [NCS Release]", callback=Flashes)
+    dpg.add_button(label="Different Heaven & EH!DE - My Heart [NCS Release]", callback=myheart)
+    dpg.add_button(label="Unknown Brain - Perfect 10 (feat. Heather Sommer)", callback=perfect10)
+    dpg.add_button(label="Egzod & Maestro Chives - Royalty (ft. Neoni) [NCS Release]", callback=Royality)
+    dpg.add_button(label="Lost Sky - Dreams", callback=Dreams)
+    dpg.add_button(label="Elektronomia - Sky High [NCS Release]", callback=skyhigh)
+    dpg.add_button(label="Cartoon - Howling (Ft. Asena) [NCS Release]", callback=howling)   
+    dpg.add_button(label="Tobu - Hope [NCS Release]", callback=hope)   
+    dpg.add_button(label="JPB - High [NCS RELEASE]", callback=high)
+    dpg.add_button(label="Jim Yosef - Eclipse [NCS Release]", callback=Eclipse)
+    dpg.add_button(label="Desmeon - Undone (feat. Steklo) [NCS Release]", callback=undone)
+    dpg.add_button(label="Ahrix - Nova", callback=nova)
+    dpg.add_button(label="Earth - K-391", callback=Earth)
+    dpg.add_button(label="DESMEON - HELLCAT [NCS RELEASE]", callback=Hellcat)
+    dpg.add_text("Other buttons :)")
+    dpg.add_button(label="Stop playing music", callback=stop)   
     dpg.add_button(label="Github", callback=git)
     dpg.add_text("Created by Zordon1337")
     
